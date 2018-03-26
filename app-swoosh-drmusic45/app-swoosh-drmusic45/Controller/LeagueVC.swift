914563fee2ab86368fef4b9bcb1002202606c0db
mobilestudio <mobilestudio@MacBook-Pro-5.local>
2018-03-26 11:34:10 -0700

    set up buttons on leaguevc

--- app-swoosh-drmusic45/app-swoosh-drmusic45/Controller/LeagueVC.swift
+++ app-swoosh-drmusic45/app-swoosh-drmusic45/Controller/LeagueVC.swift
@@ -10,14 +10,35 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    @IBOutlet weak var nextBtn: BorderButton!
+    var player: Player!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        player = Player()
     }
+    
+    //try not to build in logic to IBActions; save it for outside functions, then call functions from IBActions
     
     @IBAction func onNextButtonTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
 
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+    
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
 }
---
+++ app-swoosh-drmusic45/app-swoosh-drmusic45/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  app-swoosh-drmusic45
+//
+//  Created by mobilestudio on 3/26/18.
+//  Copyright © 2018 mobilestudio. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+}
